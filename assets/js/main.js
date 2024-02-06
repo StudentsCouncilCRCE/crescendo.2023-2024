c84a170e84861dc644468ee87e5a79044834a28a
Harsh <itsmehxrsh@gmail.com>
2024-02-06 23:45:41 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener("click", function(event) {
-      const card = event.target.closest(".page");
-      if (card) {
-        const image = card.querySelector(".image");
-        const profile = card.querySelector(".profile");
-        const name = card.querySelector(".meinname");
-        const contact = card.querySelector(".contact");
-  
-        card.classList.toggle("flip");
-        profile.classList.toggle("anima");
-        name.classList.toggle("anima");
-        contact.classList.toggle("anima");
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("click", function (event) {
+    const card = event.target.closest(".page");
+    if (card) {
+      const image = card.querySelector(".image");
+      const profile = card.querySelector(".profile");
+      const name = card.querySelector(".meinname");
+      const contact = card.querySelector(".contact");
+
+      card.classList.toggle("flip");
+      profile.classList.toggle("anima");
+      name.classList.toggle("anima");
+      contact.classList.toggle("anima");
+    }
+  });
+
+  document.addEventListener("keydown", function (e) {
+    const cards = document.querySelectorAll(".page");
+    cards.forEach((card) => {
+      if (e.keyCode === 39) {
+        card.classList.add("flip");
+      }
+      if (e.keyCode === 37) {
+        card.classList.remove("flip");
       }
     });
-  
-    document.addEventListener("keydown", function(e) {
-      const cards = document.querySelectorAll(".page");
-      cards.forEach((card) => {
-        if (e.keyCode === 39) {
-          card.classList.add("flip");
-        }
-        if (e.keyCode === 37) {
-          card.classList.remove("flip");
-        }
-      });
-    });
   });
-  
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("yes");
+  var bodyTags = document.getElementsByTagName("body");
+  console.log("yes yes");
+  for (var i = 0; i < bodyTags.length; i++) {
+    console.log("yes yes yes " + i);
+    bodyTags[i].style.background = "#000";
+  }
+  console.log("yes yes yes yes");
+});
