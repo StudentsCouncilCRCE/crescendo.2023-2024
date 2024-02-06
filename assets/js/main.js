42043551837a78ad367b059d38f56360dd14c912
Harsh <itsmehxrsh@gmail.com>
2024-02-07 00:01:50 +0530

    nig not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,28 +26,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-window.addEventListener("load", function () {
-  var iframeOverlay = document.getElementById("iframe-overlay");
-  var applyButton = document.querySelector(".apply-button");
-  var isIframeLoaded = false;
-
-  // Function to check if the apply button is converted to an iframe
-  function checkIframeConversion() {
-    var iframes = document.querySelectorAll("iframe");
-    if (iframes.length > 0) {
-      isIframeLoaded = true;
-      iframeOverlay.style.display = "block";
-    }
-  }
-
-  // Check if the apply button is converted to an iframe after a delay
-  setTimeout(checkIframeConversion, 2000); // Adjust as needed
-
-  // Hide the overlay if the iframe is loaded
-  if (isIframeLoaded) {
-    setTimeout(function () {
-      iframeOverlay.style.display = "none";
-    }, 3000); // Hides overlay after 3 seconds (adjust as needed)
-  }
-});
